885ed85a8c5b4ad6725fb861642b9ecbc1540e44
rmdayberry <dayberryrm@gmail.com>
2024-08-19 12:29:39 -0500

    Create MissionCard component
    Create MissionCard component that displays information about a mission, such as its name, status, and crew

---
+++ space-mission-control/src/MissionCard.jsx
@@ -0,0 +1,11 @@
+function MissionCard({ mission }) {
+  return (
+    <div className="mission-card">
+      <h3>{mission.name}</h3>
+      <p>Status: {mission.status}</p>
+      <p>Crew: {mission.crew}</p>
+    </div>
+  );
+}
+
+export default MissionCard;
